48080e08764632419869cf0c6c1a7e0abcc720d8
kkyouma <144501527+kkyouma@users.noreply.github.com>
2026-03-20 22:48:06 -0300

    feat: add medallion buckets to gcs

--- terraform/storage.tf
+++ terraform/storage.tf
@@ -23,3 +23,47 @@ resource "google_storage_bucket" "data_lake" {
     enabled = true
   }
 }
+
+resource "google_storage_bucket" "bronze" {
+  name          = "${var.project_id}-recast-brz"
+  location      = var.region
+  storage_class = "STANDARD"
+
+  uniform_bucket_level_access = true
+
+  lifecycle {
+    prevent_destroy = true
+  }
+
+  lifecycle_rule {
+    condition {
+      age = 7
+    }
+    action {
+      type          = "SetStorageClass"
+      storage_class = "COLDLINE"
+    }
+  }
+
+  versioning {
+    enabled = true
+  }
+}
+
+
+resource "google_storage_bucket" "silver" {
+  name          = "${var.project_id}-recast-slv"
+  location      = var.region
+  storage_class = "STANDARD"
+
+  uniform_bucket_level_access = true
+
+  lifecycle {
+    prevent_destroy = true
+  }
+
+  versioning {
+    enabled = true
+  }
+}
+
