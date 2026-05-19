60da60820f82fbb4ab46f16dfac2e2da2077a39a
kkyouma <144501527+kkyouma@users.noreply.github.com>
2026-05-19 19:22:57 -0400

    refactor: rename table to avoid duplicate/destroy the actual table

--- terraform/bigquery.tf
+++ terraform/bigquery.tf
@@ -10,7 +10,7 @@ resource "google_bigquery_dataset" "marts" {
   delete_contents_on_destroy = true
 }
 
-resource "google_bigquery_table" "generacion_real" {
+resource "google_bigquery_table" "readings" {
   dataset_id               = google_bigquery_dataset.staging.dataset_id
   table_id                 = "generacion_real"
   require_partition_filter = true
