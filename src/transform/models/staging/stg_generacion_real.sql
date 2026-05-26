0826ab587c7b8941feaec91435040bbbb53f18c4
kkyouma <144501527+kkyouma@users.noreply.github.com>
2026-05-26 18:15:14 -0400

    feat: add basic cleaning stage table

--- src/transform/models/staging/stg_generacion_real.sql
+++ src/transform/models/staging/stg_generacion_real.sql
@@ -1,1 +1,37 @@
-select * from {{ source('raw', 'generacion_real') }}
+with
+    src as (
+        select *
+        from {{ source("raw", "generacion_real") }}
+        where fecha_hora < current_timestamp()
+    ),
+
+    renamed as (
+        select
+            -- ids
+            id_central,
+
+            -- strings
+            tipo_tecnologia,
+
+            -- numerics
+            gen_real_mw as generacion_real_mw,
+            potencia_maxima as potencia_maxima_mw,
+
+            -- timestamps
+            fecha_hora as timestamp_local
+
+        from src
+    ),
+
+    casted as (
+        select
+            id_central,
+            tipo_tecnologia,
+            generacion_real_mw,
+            potencia_maxima_mw,
+            timestamp_local
+        from renamed
+    )
+
+select *
+from casted
